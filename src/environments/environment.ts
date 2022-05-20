bc06f7c3d427d92fde94645138828022e88322b8
Kasugara <Ertanto_andre11@yahoo.com>
2022-05-20 10:15:38 +0700

    basic angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl1: 'http://127.0.0.1:8080/BelowTheTide-0.0.1-SNAPSHOT',
+  apiBaseUrl: 'http://localhost:8081'
+  // apiBaseUrl:
 };
 
 /*
